f7d522f19555f871992d991271cd39c704d562d9
AMS Music School <ams.guitar.school@gmail.com>
2026-03-28 16:17:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ams-player-cache-v1.2.1';
+const CACHE_NAME = 'ams-player-cache-v1.2.2';
 const urlsToCache = [
   './',
   './index.html',
@@ -14,6 +14,10 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // FirebaseやYouTubeの外部通信はキャッシュしない設定
+  if (event.request.url.includes('firebase') || event.request.url.includes('googleapis')) {
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
